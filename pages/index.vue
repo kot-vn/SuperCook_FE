5968fb6d5e7dc2a4a5777e5f23371ee887c92545
Kien Vu <kvstudio.154@gmail.com>
2022-12-14 20:08:16 +0700

    test base

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <Tutorial />
+  <div>Test</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
 }
 </script>
+
+<style lang="scss" scoped></style>
