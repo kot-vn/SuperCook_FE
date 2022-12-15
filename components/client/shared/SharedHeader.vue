8e00d8dc3c52d52cc629d725164254c7bb976e51
Kien Vu <kvstudio.154@gmail.com>
2022-12-15 22:18:07 +0700

    test layout (#3)

---
+++ components/client/shared/SharedHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="header-wrapper">
+      <div class="brand-name">
+        <h1>SuperCook</h1>
+      </div>
+      <div class="search-bar">
+        <b-form-input
+          v-model="searchValue"
+          placeholder="Find..."
+        ></b-form-input>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/client.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <SharedHeader></SharedHeader>
+    <h1>test</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import SharedHeader from '~/components/client/shared/SharedHeader.vue'
+
+export default {
+  data() {
+    return {
+      title: 'SuperCook - Zero Waste Recipe',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+  mounted() {},
+  methods: {},
+  components: { SharedHeader },
+}
+</script>
+
+<style lang="scss" scoped></style>
