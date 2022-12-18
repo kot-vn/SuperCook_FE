b98ed803874ea5966ef171c4cb6625deb8533696
Kien Vu <116904513+behemoth-kienvt@users.noreply.github.com>
2022-12-18 14:40:07 +0700

    Update configs (#9)
    * update configs
    
    * update configs
    
    Co-authored-by: Kien Vu <kvstudio.154@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,9 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/SuperCook_FE/favicon.png' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
