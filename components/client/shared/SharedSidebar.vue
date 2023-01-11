d361504dc27dd007e3bbc3d4cd1c07781b0f783c
Kien Vu <kvstudio.154@gmail.com>
2023-01-11 22:47:24 +0700

    rollback

---
+++ components/client/shared/SharedSidebar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <b-sidebar
+      id="sidebar"
+      no-header
+      :bg-variant="sidebarBG"
+      :backdrop="showBackdrop"
+      width="450px"
+      :visible="visible"
+    >
+      <div class="sidebar-wrapper p-4">
+        <div class="brand-name pb-4">
+          <h1 class="mx-auto text-white">Pantry</h1>
+        </div>
+        <div
+          v-if="showBackdrop"
+          v-b-toggle.sidebar
+          class="close-sidebar-btn text-white position-absolute"
+        >
+          <span class="mdi mdi-close"></span>
+        </div>
+        <div class="search-bar position-relative">
+          <b-form-input
+            v-model="ingredientSearchValue"
+            class="pl-5"
+            type="search"
+            placeholder="Add/remove ingredients..."
+          >
+          </b-form-input>
+          <span class="mdi mdi-magnify position-absolute"></span>
+        </div>
+      </div>
+      <div class="sidebar-body overflow-auto p-4">
+        <IngredientCategory
+          :ingredient-categories="ingredientCategories.data"
+        ></IngredientCategory>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+<script>
+import { createNamespacedHelpers } from 'vuex'
+import IngredientCategory from '../IngredientCategory.vue'
+
+const { mapState } = createNamespacedHelpers('client')
+
+export default {
+  components: { IngredientCategory },
+  data() {
+    return {
+      visible: true,
+      showBackdrop: false,
+      ingredientSearchValue: null,
+      sidebarBG: 'unset',
+    }
+  },
+  computed: {
+    ...mapState(['ingredientCategories']),
+  },
+  created() {
+    this.showSidebar()
+  },
+  mounted() {},
+  methods: {
+    showSidebar() {
+      const screenWidth = screen.width
+      if (screenWidth <= 900) {
+        this.visible = false
+        this.showBackdrop = true
+        this.sidebarBG = 'white'
+      }
+    },
+  },
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-search-result overflow-auto">
+    <h2 class="py-4">You can make 30 recipes</h2>
+    <div class="recipe-list">
+      <div
+        v-for="(item, index) in 30"
+        :key="index"
+        class="w-50 px-2 d-inline-block"
+      >
+        <RecipeItem></RecipeItem>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import RecipeItem from '@/components/client/RecipeItem.vue'
+export default {
+  components: {
+    RecipeItem,
+  },
+  layout: 'client',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+<style lang="scss" scoped></style>
